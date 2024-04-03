1349677a3557b609969439ce3389d13c656e3cfd
My Truong <ngocmytrg@gmail.com>
2024-04-03 13:44:37 -0500

    initial

---
+++ src/test/java/com/example/eaapril2024springdata/EaApril2024SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eaapril2024springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaApril2024SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
